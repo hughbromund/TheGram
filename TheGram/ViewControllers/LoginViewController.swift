fa17b84b1be318dc567ad3043cfb6baf9468328d
Hugh Bromund <hughbromund44@gmail.com>
2020-02-17 19:24:28 -0500

    Started work on login screen

--- TheGram/ViewControllers/LoginViewController.swift
+++ TheGram/ViewControllers/LoginViewController.swift
@@ -9,9 +9,40 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var signinButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+//    Color Pallet: https://colorhunt.co/palette/165293
+    
+    let orange = UIColor(red:1.00, green:0.45, blue:0.08, alpha:1.0)
+    let white = UIColor(red:0.96, green:0.96, blue:0.96, alpha:1.0)
+    let grey = UIColor(red:0.23, green:0.21, blue:0.21, alpha:1.0)
+    let darkGrey = UIColor(red:0.14, green:0.13, blue:0.13, alpha:1.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        signinButton.backgroundColor = orange
+        signinButton.tintColor = orange
+        signinButton.setTitleColor(white, for: UIControl.State.normal)
+        
+        self.view.backgroundColor = darkGrey
+        
+        titleLabel.textColor = white
+        textLabel.textColor = white
+        usernameLabel.textColor = white
+        passwordLabel.textColor = white
+        
+        usernameTextfield.backgroundColor = white
+        passwordTextfield.backgroundColor = white
+        
+        signupButton.setTitleColor(orange, for: UIControl.State.normal)
 
         // Do any additional setup after loading the view.
     }
